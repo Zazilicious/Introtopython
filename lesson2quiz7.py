818727b3cfa6266d7d19fcbd388f9897454b69c5
Zazilicious <63361013+Zazilicious@users.noreply.github.com>
2020-04-09 15:49:00 -0400

    Create lesson2quiz7.py

---
+++ lesson2quiz7.py
@@ -0,0 +1,13 @@
+# Browse the complete list of string methods at:
+# https://docs.python.org/3/library/stdtypes.html#string-methods
+# and try them out here
+fish = "fucking delicious"
+print(fish.capitalize())
+
+# Write two lines of code below, each assigning a value to a variable
+person = "Lexi"
+action = "ate"
+
+# Now write a print statement using .format() to print out a sentence and the 
+#   values of both of the variables
+print("Godzilla {} {}".format(action, person))
