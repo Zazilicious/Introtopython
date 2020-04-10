a68b8a51481c34439a48305e543183751acdbec8
Zazilicious <63361013+Zazilicious@users.noreply.github.com>
2020-04-10 00:09:29 -0400

    Create lesson3quiz2.py

---
+++ lesson3quiz2.py
@@ -0,0 +1,8 @@
+# Use this playground to experiment with list methods, using Test Run
+IW = ["Kind", "thoughtful", "smart", "amazing"]
+print(sorted(IW))
+IW.append("Incredible")
+print(sorted(IW))
+print(min(IW))
+print(max(IW))
+print(len(IW))
