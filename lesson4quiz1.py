8b736087ec13927d42818979ff2d9353709591fd
Zazilicious <63361013+Zazilicious@users.noreply.github.com>
2020-04-13 17:06:54 -0400

    Create lesson4quiz1.py

---
+++ lesson4quiz1.py
@@ -0,0 +1,12 @@
+points = 174  # use this input to make your submission
+
+# write your if statement here
+if points <=50:
+    result = "Congratulations! You won a wooden rabbit!"
+elif points <=150:
+    result = "Oh dear, no price this time."
+elif points <=180:
+    result = "Congratulations! You won a wafer-thin mint!"
+else:
+    results = "congratulations! You won a penguin!"
+print(result)
