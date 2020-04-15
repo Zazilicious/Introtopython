8bf70bb3d1009d559e05412a51927329e3081cea
Zazilicious <63361013+Zazilicious@users.noreply.github.com>
2020-04-15 17:54:15 -0400

    Create lesson4quiz7.py

---
+++ lesson4quiz7.py
@@ -0,0 +1,35 @@
+# number to find the factorial of
+number = 6   
+
+# start with our product equal to one
+product = 1
+
+# track the current number being multiplied
+current = 1
+
+# write your while loop here
+while current <= number:
+    
+    # multiply the product so far by the current number
+    product = product * current
+    
+    # increment current with each iteration until it reaches number
+    current = current + 1
+
+
+# print the factorial of number
+print(product)
+
+# number to find the factorial of
+number = 6   
+
+# start with our product equal to one
+product = 1
+
+# write your for loop here
+for i in range(number):
+    product = (i + 1) * product
+
+
+# print the factorial of number
+print(product)
