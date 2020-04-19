fc5601bc0e1c9153ec240e149532498adfeaf038
Zazilicious <63361013+Zazilicious@users.noreply.github.com>
2020-04-18 20:53:18 -0400

    Create lesson6quiz6.py

---
+++ lesson6quiz6.py
@@ -0,0 +1,25 @@
+# Write your code here
+def create_flowerdict(filename):
+    flower_dict = {}
+    with open(filename) as f:
+        for line in f:
+            letter = line.split(": ")[0].lower() 
+            flower = line.split(": ")[1].strip()
+            flower_dict[letter] = flower
+    return flower_dict
+
+# HINT: create a dictionary from flowers.txt
+
+# HINT: create a function to ask for user's first and last name
+def main(): 
+    flower_d = create_flowerdict('flowers.txt')
+    full_name = input("Enter your First [space] Last name only: ")
+    first_name = full_name[0].lower()
+    first_letter = first_name[0]
+# print command that prints final input with value from corresponding key in dictionary
+    print("Unique flower name with the first letter: {}".format(flower_d[first_letter]))
+
+main()
+# print the desired output
+
+
